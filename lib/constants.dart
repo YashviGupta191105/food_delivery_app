788ce6f9d319f42c835aebefb44484da85668c0e
Yashvi Gupta <yashvi135@gmail.com>
2024-11-16 04:39:29 +0530

    Initial commit

---
+++ lib/constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColour = Color.fromARGB(255, 93, 41, 148);
+const kPrimaryLightColour = Color.fromARGB(255, 210, 209, 211);
+
+const double kDefaultPadding = 16.0;
---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:food_delievery_app/constants.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title : 'DineDoor',
+      theme: ThemeData(
+        primaryColor: kPrimaryColour,
+        scaffoldBackgroundColor: Colors.white,
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            elevation: 0,
+            backgroundColor: kPrimaryColour,
+            shape: const StadiumBorder(),
+            minimumSize: const Size (double.infinity, 56),
+            maximumSize: const Size (double.infinity, 56),
+          ) ),
+          inputDecorationTheme: const InputDecorationTheme(
+            filled : true,
+            fillColor: kPrimaryLightColour,
+            prefixIconColor: kPrimaryColour,
+            contentPadding: EdgeInsets.symmetric(horizontal: kDefaultPadding, vertical: kDefaultPadding),
+
+            border: OutlineInputBorder(
+              borderSide: BorderSide.none,
+              borderRadius: BorderRadius.all(Radius.circular(30)),
+            )
+          )
+      ),
+     // home: const WelcomeScreen(),
+
+    );
+  }
+}
