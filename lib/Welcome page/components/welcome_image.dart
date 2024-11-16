d772520e6e24abca97499159433a170292957b18
Yashvi Gupta <yashvi135@gmail.com>
2024-11-16 20:24:20 +0530

    Changes done

---
+++ lib/Welcome page/components/welcome_image.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:food_delievery_app/constants.dart';
+
+class WelcomeImage extends StatelessWidget {
+  const WelcomeImage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(
+          "Welcome to DineDoor",
+          style: Theme.of(context).textTheme.headlineLarge!.copyWith(
+             color: Colors.black,
+             fontWeight: FontWeight.bold,
+          ),
+          
+        ),
+        SizedBox(height: kDefaultPadding * 2),
+        Row(
+          children: [
+            Spacer(),
+            Container(
+              width: 300,
+              height: 300,
+              child: SvgPicture.asset("assets/icons/foodlogo.svg") ,
+            )
+          ],
+        )
+      ],
+    );
+  }
+}
